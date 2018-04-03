426ec6e28aa7724efd58b2cb2cae598be6b1f134
Sandeep Gottimukkala <sgottimukkal@tamu.edu>
2018-04-03 18:49:07 -0500

    Remove warnings.

--- Developer/developer.py
+++ Developer/developer.py
@@ -3,25 +3,25 @@ while True:
     cmd = raw_input("Enter your command:")
     cmd_parts = cmd.split(' ')
 
-    if(cmd_parts[0] == "quit"):
+    if cmd_parts[0] == "quit":
         break;
 
-    if(cmd_parts[0] != "connect" and !connected):
+    if cmd_parts[0] != "connect" and  not connected :
         print("Please first connect to the lambda service.")
         continue
 
-    if(cmd_parts[0] == "connect"):
+    if cmd_parts[0] == "connect":
         connected = True
         continue
 
-    if(cmd_parts[0] == "setup"):
+    if cmd_parts[0] == "setup":
         #setup account with the lambda service
         continue
 
-    if(cmd_parts[0] == "login"):
+    if cmd_parts[0] == "login":
         #login to the service
         continue
 
-    if(cmd_parts[0] == "deploy"):
+    if cmd_parts[0] == "deploy":
         #deploy files to the lamdba service
         continue
