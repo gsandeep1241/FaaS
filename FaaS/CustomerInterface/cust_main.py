c7b1e00882d315a3ae6f1a4ddcf3170849434626
gskesav@tamu.edu <gskesav@tamu.edu>
2018-04-09 01:12:46 -0500

    user functionality

--- FaaS/CustomerInterface/cust_main.py
+++ FaaS/CustomerInterface/cust_main.py
@@ -5,5 +5,26 @@ def process_customer_input():
 		if cmd == "quit":
 			print ("Goodbye!")
 			break
+			
+		cmdps = cmds.split(" ")
+		
+		if len(cmdps) == 2 and cmdps[0] == "get":
+			## initiate get request
+			L = 0
+			
+		elif len(cmdps) == 3 and cmdps[0] == "put":
+			## Initiate put request
+			L = 0
+		
+		elif len(cmdps) == 3 and cmdps[0] == "post":
+			## Initiate post request
+			L = 0
+			
+		elif len(cmdps) == 2 and cmdps[0] == "delete":
+			## Initiate delete request
+			L = 0
+			
+		else:
+			print("Invalid Command, enter again")
 
 	return "Test"
