dc333ff2e8029de576905dd429e1532d094e5553
gskesav@tamu.edu <gskesav@tamu.edu>
2018-04-08 22:08:04 -0500

    Login and Signup structure

--- FaaS/DeveloperInterface/dev_main.py
+++ FaaS/DeveloperInterface/dev_main.py
@@ -1,8 +1,26 @@
 def process_developer_input():
+
+	## Loading the user credentials map
+	
+
 	while True:
 		cmd = raw_input('Enter your command:')
 
-		if cmd == "quit":
+		cmdps = cmd.split(" ")
+		
+		if cmdps[0] == "quit":
 			print ("Goodbye!")
 			break
+			
+		elif cmdps[0] == "login" and len(cmdps) == 3:
+			username = cmdps[1]
+			password = cmdps[2]
+			
+		elif cmdps[0] == "signup" and len(cmdps) == 3:
+			username = cmdps[1]
+			password = cmdps[2]
+			
+		else:
+			print("Invalid Command")
+		
 	return "Test"
