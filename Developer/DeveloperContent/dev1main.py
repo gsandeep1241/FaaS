f822945f480d29ba1b308428179d0eb16c429ce8
gskesav@tamu.edu <gskesav@tamu.edu>
2018-04-16 18:23:50 -0500

    setup DB for developer functions

--- Developer/DeveloperContent/dev1main.py
+++ Developer/DeveloperContent/dev1main.py
@@ -1,5 +1,17 @@
 import pickle
 
+ID = "YTZRQP"
+db_base_path = os.path.abspath(os.path.join(os.path.dirname(__file__),'../../Database/'))
+db_path = os.path.join(db_base_path, (ID+'.pckl'))
+
+try:
+	with open(db_path, 'rb') as f:
+		dogs = pickle.load(f)
+		f.close()
+	
+except IOError:
+	dogs = {}
+
 def add(data):
 	return data
 
