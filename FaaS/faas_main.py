4a4c197627af662bfe0ae2642daac0c9a0d268a6
gskesav@tamu.edu <gskesav@tamu.edu>
2018-04-16 01:24:26 -0500

    Processing queue events

--- FaaS/faas_main.py
+++ FaaS/faas_main.py
@@ -6,6 +6,7 @@ from os.path import isfile, join
 import glob
 
 read_path = os.path.abspath(os.path.join(os.path.dirname(__file__),'CustomerInterface/WriteContent/'))
+write_path = os.path.abspath(os.path.join(os.path.dirname(__file__),'CustomerInterface/ReadContent/'))
 
 def does_file_exist_in_dir(path):
 	return any(isfile(join(path, i)) for i in listdir(path))
@@ -14,7 +15,20 @@ while True:
 	if not does_file_exist_in_dir(read_path):
 		continue
 	
+	file_name = ""
 	for infile in sorted(os.listdir(read_path)):
-		print infile 
-		os.remove(os.path.join(read_path, infile))
+		file_name = os.path.join(read_path, infile)
+		event_obj = {}
+		with open(file_name, 'rb') as f:
+			event_obj = pickle.load(f)
+			f.close()
+			
+		os.remove(file_name)
+		file_name = os.path.join(write_path, infile)
+		f = open(file_name, 'wb')
+		pickle.dump(event_obj, f)
+		f.close()
+		print(infile + " file removed")
 		break
+		
+	break
