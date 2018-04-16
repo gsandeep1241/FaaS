28f350e6b2f39989d6b434c4bacd6d6aaef98666
Sandeep Gottimukkala <sgottimukkal@tamu.edu>
2018-04-15 23:54:14 -0500

    Handling customer get operation

--- FaaS/CustomerInterface/cust_main.py
+++ FaaS/CustomerInterface/cust_main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import datetime
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'../')))
 import event_object_generator as eog
@@ -20,10 +21,18 @@ def process_customer_input():
 			continue
 		
 		if len(cmdps) == 2 and cmdps[0] == "get":
-			# generate event object
-			event_object = eog.generate_object(cmd)
 			## initiate get request
-			L = 0
+			# generate time stamp and place event_object in a pckl file and put it in some destination with file name as event object
+			#start a listener here to look for a file in a new location with  file name as the timestamp
+			
+			file_name = str(datetime.datetime.now()) + '.pckl'
+			file = os.path.abspath(os.path.join(os.path.dirname(__file__),'WriteContent/' + filename))
+			
+			f = open(file, 'wb')
+			pickle.dump(event_object, f)
+			f.close()
+			
+			#write listener here
 			
 		elif len(cmdps) == 3 and cmdps[0] == "put":
 			## Initiate put request
